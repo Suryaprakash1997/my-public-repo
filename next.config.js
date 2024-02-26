d5382492cbbbee0dc06d8fd7e351cc0eb320fb46
Suryaprakash1997 <157590817+Suryaprakash1997@users.noreply.github.com>
2024-02-26 12:38:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+/** @type {import('next').Dynamo} */
+const Dynamo = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['tailwind-config', 'ui'],
+};
+
+module.exports = Dynamo;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,3 +15,12 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/** @type {import('next').db} */
+const db = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['tailwind-config', 'ui'],
+};
+
+module.exports = db;
